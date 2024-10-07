7f22a9a9d73be6da3193e5ba30f9be7b20fd0dbd
E-Boyn <elytza.boyn@gmail.com>
2024-10-07 02:07:00 +0200

    Worked out how to display an image
    Will probably need to make another class for Images

--- Card.java
+++ Card.java
@@ -1,19 +1,28 @@
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.event.*;
 
 //TODO Make this class abstract
 public class Card extends JFrame {
-    Card(){
+    JLabel diplayImage;
+    ImageIcon image;
+    Dimension size = new Dimension();
+    String imagePath = "";
 
+    Card(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         this.setUndecorated(true);
         this.addFocusListener(new CustomFocusListener());
 
-        this.setVisible(true); // TODO overite in each enemy card
-        this.setSize(400, 300); //DElEte this, overrite it in each subclass - use % of the screen resolution
+        this.setVisible(true); // TODO overite in each enemy card?
+        this.setSize(400, 300); //TODO DElEte this, overrite it in each subclass - use % of the screen resolution
         
+        image = new ImageIcon(getClass().getResource("images\\house1.png"));
+        diplayImage = new JLabel(image);
+        this.add(diplayImage);
     }
 }
--- main.java
+++ main.java
@@ -1,17 +1,22 @@
-
-import java.awt.Color;
+import java.awt.*;  
+import javax.swing.JFrame;
+import javax.swing.*;
 
 /**
  * main
  */
 public class main{
 public static void main(String[] args) {
+    //for now just testing how stiff works, nothing important here
+    Canvas m = new Canvas();    
     Card card = new Card();
-    Card card2 = new Card();
-    Card card3 = new Card();
-    card2.setSize(700, 800);
-    card.getContentPane().setBackground(Color.black);
-    card3.getContentPane().setBackground(Color.red);
-    card3.setSize(1000, 200);
+    
+    card.setSize(800, 800);
+
+    //Card card2 = new Card();
+    //Card card3 = new Card();
+    //card.getContentPane().setBackground(Color.black);
+   // card3.getContentPane().setBackground(Color.red);
+    //card3.setSize(1000, 200);
 }
 }
