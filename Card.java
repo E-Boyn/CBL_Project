18b9743bc8e05d0d8073a5ae5642d043eb24a9dc
E-Boyn <elytza.boyn@gmail.com>
2024-10-10 10:52:46 +0200

    Making changes of the core structure
    Deleted the Focus Listener class.
    * Instead used implemented on the Card class.
    * Added a mouse movement listener to card

--- Card.java
+++ Card.java
@@ -6,22 +6,53 @@ import java.awt.*;
 import java.awt.event.*;
 
 //TODO Make this class abstract
-public class Card extends JFrame {
+public class Card extends JFrame implements FocusListener, MouseMotionListener{
+    @Override
+    public void focusGained(FocusEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+        // TODO Auto-generated method stub
+    }
+//---------------------
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        mousePosition = new Point(e.getX() , e.getY());
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+    }
+
+//===================================================================
+    Point mousePosition; //position of Mouse while it's being dragged (pressed + moved)
     JLabel diplayImage;
     ImageIcon image;
     Dimension size = new Dimension();
     String imagePath = "";
+    boolean visible = true;
+//==================================================================
+
 
     Card(){
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         this.setUndecorated(true);
-        this.addFocusListener(new CustomFocusListener());
-
-        this.setVisible(true); // TODO overite in each enemy card?
+        this.addFocusListener(this);
+        this.addMouseMotionListener(this);
+        this.setVisible(visible); // TODO overite in each enemy card?
         this.setSize(400, 300); //TODO DElEte this, overrite it in each subclass - use % of the screen resolution
         
-        image = new ImageIcon(getClass().getResource("images\\house1.png"));
+        
+    }
+
+    
+    public void setVisible(boolean b){
+        visible = b;
+    }
+
+    public void SetImage(String path) {
+        image = new ImageIcon(path);
         diplayImage = new JLabel(image);
         this.add(diplayImage);
     }
