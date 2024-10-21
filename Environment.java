c8f85c0c9a8bb93cde38cce152b2b295154a9cf3
4Leia-git <a.r.wilis@student.tue.nl>
2024-10-21 12:36:20 +0200

    Correcting comments
    Turns out this wasn't changed in the previous commit (probably delayed)

--- Environment.java
+++ Environment.java
@@ -84,7 +84,7 @@ public class Environment {
         List<Rectangle> environmentBounds = new ArrayList<>();
         int safeZoneHeightOffset = 150;  // Place cards ABOVE Player/Dagger
     
-        // Step 1: Generate the environment cards' positions first, but do not display them yet
+        // Generate environment cards' positions first, but not display them yet
         for (Card environmentCard : environmentCards) {
             // Use randomPosition() to get random coordinates
             Point randomPos = environmentCard.randomPosition(
@@ -92,30 +92,31 @@ public class Environment {
                 screenHeight - safeZoneHeightOffset - environmentCard.getHeight()
             );
     
-            // Track the bounds of each environment card for later use
+            // Track bounds of each environment card for later use
             environmentBounds.add(new Rectangle(randomPos.x, randomPos.y,
                 environmentCard.getWidth(), environmentCard.getHeight()));
             
-            // Temporarily store the position but don't make the environment card visible yet
+            // Temporarily store position but don't make the environment card visible yet
             environmentCard.setLocation(randomPos.x, randomPos.y);
         }
     
-        // Step 2: Pick one of the environment cards to hide the enemy card inside (top-left point)
+        // Pick random environment card to hide the enemy card inside
+        // Using its top-left point to generate enemy so it can be covered
         if (enemyFlag) {
             Rectangle chosenEnv = environmentBounds.get(rand.nextInt(environmentBounds.size()));
     
-            // Place the enemy card at the top-left corner of the chosen environment card
+            // Place enemy card at the top-left corner of the chosen environment card
             Point enemyPos = getTopLeftPointInside(chosenEnv);
             enemyCard.setLocation(enemyPos.x, enemyPos.y);
             enemyCard.setVisible(true);  // Display the enemy card, but it will be covered later
         }
     
-        // Step 3: Now make the environment cards visible (they will cover the enemy card)
+        // Make environment cards visible (they will cover enemy card)
         for (Card environmentCard : environmentCards) {
             environmentCard.setVisible(true);  // Now show the environment cards to cover the enemy
         }
     
-        // Step 4: Handle treasure similarly if needed (using top-left point)
+        // Use later ---- Handle treasure similarly
         if (treasureFlag) {
             Rectangle chosenEnv = environmentBounds.get(rand.nextInt(environmentBounds.size()));
             Point treasurePos = getTopLeftPointInside(chosenEnv);
@@ -124,10 +125,9 @@ public class Environment {
         }
     }
     
-    
+
     // Helper method to get the top-left point of a given rectangle (environment card bounds)
     private Point getTopLeftPointInside(Rectangle rect) {
-        // Top-left point of the rectangle is simply its x and y coordinates
         return new Point(rect.x, rect.y);
     }
 
