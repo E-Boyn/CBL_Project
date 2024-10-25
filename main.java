b256df7efa10d4141a8b1a7e17f566db81988d39
E-Boyn <elytza.boyn@gmail.com>
2024-10-25 23:16:51 +0200

    Renamed Main to main.

--- main.java
+++ main.java
@@ -3,10 +3,7 @@ import javax.swing.SwingUtilities;
 /** The main entry point for the game.
  * Starts the game with round 1 (index 0 --> round 0).
  */
-public class Main { // "The name of the outer type and the file do not match." when it does match???
-
-    public Main() {
-    }
+public class main { // "The name of the outer type and the file do not match." when it does match???
 
     public static void main(String[] args) {
 
