98802f802459f46a0d7c56c24eebe9052e04e0b6
E-Boyn <elytza.boyn@gmail.com>
2024-10-27 19:14:14 +0100

    Rename Main to main

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ import javax.swing.SwingUtilities;
 /** The main entry point for the game.
  * Starts the game with round 1 (index 0 --> round 0).
  */
-public class Main {
+public class main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             Environment gameEnvironment = new Environment(0);  // Start with round 0
